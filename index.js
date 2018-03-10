56ba2bf8e39ef148f9e5631886aa575c530df74f
yangronghao <yrhrh0@gmail.com>
2018-03-10 10:28:41 +0630

    add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+import './index.css';
+import AppRoutes from './AppRoutes';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <AppRoutes/>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker();
